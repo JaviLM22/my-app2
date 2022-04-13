5225e6cf9c7e87ce575748dfc51d7c3b761e4566
JaviLM22 <javier.lpz.mrtn@gmail.com>
2022-04-13 22:41:03 +0200

    cambios de componentes

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import {BrowserRouter, Route, Routes} from "react-router-dom"
+import Home from './componentes/Home'
+import Facturas from './componentes/Facturas'
+import Clientes from './componentes/Clientes'
+import Productos from './componentes/Productos'
 
 ReactDOM.render(
 <BrowserRouter>
   <Routes>
-    <Route path="/" element={ <Home></Home>}>
-    <Route path="/facturas" element={ <Facturas></Facturas>}>
-    <Route path="/productos" element={ <Clientes></Clientes>}>
-    <Route path="/clientes" element={ <Productos></Productos>}>
+    <Route path="/" element={ <Home></Home>} >
+     <Route path="/facturas" element={ <Facturas></Facturas>} />
+     <Route path="/clientes" element={ <Clientes></Clientes>} />
+     <Route path="/productos" element={ <Productos></Productos>} />
     </Route>
   </Routes>
 </BrowserRouter>,
